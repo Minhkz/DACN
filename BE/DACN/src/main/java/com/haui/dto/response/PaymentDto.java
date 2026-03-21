c7a7b305cd2a3ebe974465afd4ccee3e9bd2e858
nvu68 <vuquocnguyen2k5@gmail.com>
2026-03-21 16:43:19 +0700

    Add files via upload

---
+++ BE/DACN/src/main/java/com/haui/dto/response/PaymentDto.java
@@ -0,0 +1,11 @@
+package com.haui.dto.response.payment;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class PaymentDto {
+    private String status;
+    private String message;
+    private String paymentUrl;
+}
