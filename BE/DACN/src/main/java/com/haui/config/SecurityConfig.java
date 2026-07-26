2c1454171187eb0af0759c1f4590ea51fc7392a3
Minhkz <minhluky0@gmail.com>
2026-07-26 18:05:42 +0700

    add domain prod

--- BE/DACN/src/main/java/com/haui/config/SecurityConfig.java
+++ BE/DACN/src/main/java/com/haui/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:1010", "http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:1010", "http://localhost:3000", "https://dacn-*-minhkzs-projects.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "Cache-Control", "X-Requested-With"));
         config.setExposedHeaders(List.of("Authorization"));
